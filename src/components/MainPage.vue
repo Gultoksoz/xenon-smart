c26cc245e1fa7a84fedadf88cc6da0100a3c9dc8
Meryem Gul Toksoz <meryem.toksoz@xenonsmart.com>
2023-11-10 17:15:31 +0300

    ilk dizayn

---
+++ src/components/MainPage.vue
@@ -0,0 +1,84 @@
+<template>
+   <v-container fluid class="main">
+    <v-row no-gutters>
+      <v-col cols="6" md="6" align="center" justify="center">
+        <v-card class="left-aligned-card" variant="text">
+          <v-card-title><strong>Akıllı ev sistemleri, </strong><br>modern yaşamı yeniden tanımlayın.</v-card-title>
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </v-card-text>
+          <v-card-action>
+
+            <v-col cols="4" sm="6" md="4">
+            <v-btn block rounded="xl" size="small"  variant="outlined" color="primary">
+              Ürünleri incele
+            </v-btn></v-col>
+          </v-card-action>
+        </v-card>
+    </v-col>
+    <v-col cols="6" md="6">
+      <v-img class="img"
+        :width="650"
+        aspect-ratio="4/3"
+        cover
+        src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+      ></v-img>
+    </v-col>
+  </v-row>
+  <v-row class="d-flex flex-column">
+    <v-spacer>
+      <v-col cols="12" class="d-flex align-center justify-center">
+        
+        <v-card  width="100px" height="100px" 
+           class="d-flex align-center justify-center mr-4">
+           <v-row class="d-flex align-center justify-center mt-4 ">
+           <v-icon size="48" icon="mdi mdi-home-outline"></v-icon>
+           <v-card-text class="card-font"> Akıllı Yaşam</v-card-text></v-row>
+        </v-card>
+        
+        <v-card  width="100px" height="100px" class="d-flex align-center justify-center mr-4">
+          3
+        </v-card>
+        
+        <v-card width="100px" height="100px" class="d-flex align-center justify-center mr-4">
+          6
+        </v-card>
+        
+        <v-card width="100px" height="100px" class="d-flex align-center justify-center mr-4">
+          12
+        </v-card>
+        
+        <v-card width="100px" height="100px" class="d-flex align-center justify-center mr-4">
+          6
+        </v-card>
+        
+        <v-card width="100px" height="100px" class="d-flex align-center justify-center mr-4">
+          12
+        </v-card>
+      </v-col>
+    </v-spacer>
+  </v-row>
+   </v-container>
+</template>
+  
+<script >
+    
+</script>
+
+<style scoped >
+.left-aligned-card {
+  text-align: left;
+  padding-left: 20px;
+}
+.main{
+  padding: 0px;
+}
+.img{
+  margin-left: auto;
+}
+.card-font{
+  font-size: 11px;
+}
+
+</style>
+  
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-app-bar app flat  height="150">
+    
+      <v-img
+      class="logo"
+        src="@/assets/Xenon-Logo.png" 
+        max-height="40px" 
+        max-width="120px" 
+        contain
+      />
+    
+    <v-toolbar-title class="menu">
+      
+      <v-sheet class="chips"> 
+        <v-chip text-color="black" @click="" variant="text" class="custom-chip">Ürünler
+          <v-icon end icon="mdi mdi-chevron-down"></v-icon>
+        </v-chip>
+        <v-chip text-color="black" @click="" variant="text" class="custom-chip">Online Mağaza</v-chip>
+        <v-chip text-color="black" @click="" variant="text" class="custom-chip">Destek</v-chip>
+        <v-chip text-color="black" @click="" variant="text" class="custom-chip">İletişim</v-chip>
+      </v-sheet>
+    </v-toolbar-title>
+
+    
+
+    <v-list >
+        <v-list-group value="languageOptions">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              v-bind="props"
+              title="TR"
+              append-icon="mdi mdi-chevron-down"
+            ></v-list-item>
+          </template>
+
+          <v-list-item
+            
+            title="TR"
+            value="TR"
+          ></v-list-item>
+
+          <v-list-item
+           
+            title="EN"
+            value="EN"
+          ></v-list-item>
+
+        </v-list-group>
+      </v-list>
+  <v-icon end icon="mdi mdi-web"></v-icon>
+  </v-app-bar>
+  
+  <v-divider :thickness="151"></v-divider> 
+
+</template>
+
+<script >
+  export default {
+  data() {
+    return {
+      
+      languageOptions: ['TR', 'EN'],
+      
+    }
+  },
+  methods: {
+   
+    }
+    
+}
+
+</script>
+<style scoped>
+
+
+.logo{
+  margin: 30px;
+}
+.chips{
+  background-color: #e6e6e6;
+  border-radius: 20px;
+  padding:5px 10px 5px 10px;
+
+}
+.menu{
+  display: flex;
+  justify-content: center;
+}
+
+.custom-chip:hover {
+  background-color: #fff;
+ 
+}
+
+.custom-chip--active {
+  background-color: #fff; 
+  
+}
+
+</style>
