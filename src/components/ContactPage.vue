8a49ccf2faeda3f3fb0b963bb8d8570f8987c0d0
Meryem Gul Toksoz <meryem.toksoz@xenonsmart.com>
2023-11-21 14:18:29 +0300

    page design contact page

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -18,8 +18,8 @@
             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3006.0412900578685!2d28.9868288!3d41.1117893!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab54b7efb98d5%3A0xac34d0462f5f7c92!2sBili%C5%9Fim%20Vadisi%20%C4%B0stanbul!5e0!3m2!1str!2str!4v1700563845234!5m2!1str!2str" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </v-spacer>
           
-          <v-col cols="12" class="d-flex card_form">
-            <v-col cols="4" offset="8" variant="elevated" class="py-0">
+          <v-col cols="12" class="d-flex card_form justify-end">
+            <v-col cols="12" md="6" lg="4" variant="elevated" class="py-0">
               <v-card class="rounded-xl px-4 py-4">
 
               <v-card-title class="text-color">GERİ BİLDİRİM FORMU</v-card-title> 
@@ -52,24 +52,24 @@
         </v-row>
 
         <v-row class="main mb-4">
-          <v-col>
+          <v-col cols="12" sm="6" md="3">
             <strong>HQ </strong>
             <p>Ayazağa Mah. Kemerburgaz Cad. Vadi İstanbul Park Sitesi, 7A Blok  Kat:12 No:47 Sarıyer/İstanbul
             </p>
           </v-col>
 
-          <v-col>
+          <v-col cols="12" sm="6" md="3">
             <strong>R&D</strong>
             <p>Muallimköy Mah. Deniz Cad. No:143/5 A1 Blok K:11 No:84 
 Gebze, Kocaeli Türkiye</p>
           </v-col>
           
-          <v-col>
+          <v-col cols="12" sm="6" md="3">
             <strong>Telefon</strong>
             <p>+90 (850) 303 46 66</p>
           </v-col>
           
-          <v-col>
+          <v-col cols="12" sm="6" md="3">
             <strong>E-Posta</strong>
             <p>info@xenonsmart.com</p>
           </v-col>
@@ -109,6 +109,11 @@ Gebze, Kocaeli Türkiye</p>
   .card_form {
     position: absolute;
   }
+ @media screen and (max-width: 960px) {
+  .card_form {
+    position: relative;
+  }
+ }
   .small-text{
     font-size: 1em;
     color: black;
