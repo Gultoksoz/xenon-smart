0254a483166bd5a7a9b24ee8c6d34b7568206d29
Meryem Gul Toksoz <meryem.toksoz@xenonsmart.com>
2023-12-14 10:46:26 +0300

    fixed main page width

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -3,13 +3,13 @@
     <v-row class="header_element">
       <v-col cols="12" md="6">
         <v-card class="left-aligned-card" variant="text">
-          <v-spacer class="custom-card-title d-flex flex-column" style="font-size: 28px;">
+          <v-spacer class="custom-card-title d-flex flex-column" style="font-size: 51px; line-height: 60px;">
             <span class="font-weight-bold">Akıllı ev sistemleri,</span>
             <span>
               modern yaşamı yeniden tanımlayın.
             </span>
           </v-spacer>
-          <v-spacer class="mb-4 mt-2" style="font-size: 16px;">
+          <v-spacer class="mb-4 mt-2" style="font-size: 15px; ">
             <span>
               Akıllı ev sistemleri, teknolojinin en son ve en etkili halini sunarak yaşam tarzınıza değer katıyor. Evdeki her cihazın birbirine bağlı olması, geleceğin konforunu bugünden deneyimlemenizi sağlar.
             </span>
@@ -36,7 +36,7 @@
     <v-col class="d-flex flex-wrap align-center justify-space-around">
       <v-col cols="6" md="4" lg="2" >
         <v-card
-           class="d-flex align-center justify-center  custom-card" @click="$router.push('/RgbLed')">
+           class="d-flex align-center justify-center  custom-card" @click="$router.push('/RgbLed')" style=" padding-bottom: 10%; padding-top: 10%;">
            <v-card-title class="d-flex align-center justify-center flex-column">
            <v-icon > <img src="@/assets/akilli_gecis_kontrol.svg"/></v-icon>
            <v-card-text class="custom-card-text">Akıllı Geçiş Kontrol</v-card-text></v-card-title>
@@ -44,7 +44,7 @@
       </v-col>
       <v-col cols="6" md="4" lg="2">
         <v-card  
-           class="d-flex align-center justify-center  custom-card" >
+           class="d-flex align-center justify-center  custom-card" style=" padding-bottom: 10%; padding-top: 10%;" >
            <v-card-title class="d-flex align-center justify-center  flex-column">
             <v-icon > <img src="@/assets/akilli_kontrol_grubu.svg"/></v-icon>
            <v-card-text class="custom-card-text">Akıllı Kontrol Grubu</v-card-text></v-card-title>
@@ -52,7 +52,7 @@
       </v-col>
       <v-col cols="6" md="4" lg="2">
         <v-card  
-           class="d-flex align-center justify-center  custom-card" >
+           class="d-flex align-center justify-center  custom-card" style=" padding-bottom: 10%; padding-top: 10%;" >
            <v-card-title class="d-flex align-center justify-center flex-column ">
             <v-icon > <img src="@/assets/akilli_kamera.svg"/></v-icon>
            <v-card-text class="custom-card-text">Akıllı Kamera</v-card-text></v-card-title>
@@ -60,7 +60,7 @@
       </v-col>
       <v-col cols="6" md="4" lg="2">
         <v-card 
-           class="d-flex align-center justify-center  custom-card " >
+           class="d-flex align-center justify-center  custom-card " style=" padding-bottom: 10%; padding-top: 10%;" >
            <v-card-title class="d-flex align-center justify-center  flex-column">
             <v-icon > <img src="@/assets/akilli_kapi_kilidi.svg"/></v-icon>
            <v-card-text class="custom-card-text">Akıllı Kapı Kilidi</v-card-text></v-card-title>
@@ -68,7 +68,7 @@
       </v-col>
       <v-col cols="6" md="4" lg="2">
         <v-card  
-           class="d-flex align-center justify-center  custom-card" >
+           class="d-flex align-center justify-center  custom-card" style=" padding-bottom: 10%; padding-top: 10%;">
            <v-card-title class="d-flex align-center justify-center flex-column ">
             <v-icon > <img src="@/assets/akilli_sensor.svg"/></v-icon>
            <v-card-text class="custom-card-text">Akıllı Sensör</v-card-text></v-card-title>
@@ -76,7 +76,7 @@
       </v-col>
       <v-col cols="6" md="4" lg="2">
         <v-card
-           class="d-flex align-center justify-center custom-card" @click="$router.push('/smartlighting')">
+           class="d-flex align-center justify-center custom-card" @click="$router.push('/smartlighting')" style=" padding-bottom: 10%; padding-top: 10%;">
            <v-card-title class="d-flex align-center justify-center flex-column ">
             <v-icon > <img src="@/assets/akilli_aydinlatma.svg"/></v-icon>
            <v-card-text class="custom-card-text">Akıllı Aydınlatma</v-card-text></v-card-title>
@@ -89,15 +89,16 @@
   <v-row class="d-flex flex-column">
     <v-spacer class="text-center mb-3">
       <!-- <v-col cols="12" class="d-flex align-center justify-center"> -->
-        <h3 style="color: gray;">Xenon Smart Hakkında</h3>
+        <h3 style="color: gray; font-size: 20px;">Xenon Smart Hakkında</h3>
       <!-- </v-col> -->
     </v-spacer>
-    <v-spacer class="text-center">
+    <v-spacer class="d-flex justify-center">
+    <v-spacer class="text-center" style="max-width: 1000px;">
       <!-- <v-col cols="12" class="d-flex flex-column custom-text "> -->
-        <p class="font-weight-bold">Mucitler, mühendisler ve tasarımcılardan oluşan ekibimiz, akıllı ev deneyimini bir üst seviyeye taşıyan ürünler yaratmak için tutkuyla çalışıyor.</p>
-        <p >Yenilikçi teknolojimizle gurur duyuyor ve keyif anlarınızın benzersiz olmasını sağlamak için sürekli olarak mükemmellik için çalışıyoruz.</p>
+        <p class="font-weight-bold" style=" font-size: 40px; line-height: 48px;">Mucitler, mühendisler ve tasarımcılardan oluşan ekibimiz, akıllı ev deneyimini bir üst seviyeye taşıyan ürünler yaratmak için tutkuyla çalışıyor.</p>
+        <p style="font-size: 20px; padding-top: 20px;">Yenilikçi teknolojimizle gurur duyuyor ve keyif anlarınızın benzersiz olmasını sağlamak için sürekli olarak mükemmellik için çalışıyoruz.</p>
       <!-- </v-col> -->
-    </v-spacer>
+    </v-spacer></v-spacer>
   </v-row>
 
   <v-row class="d-flex flex-column">
@@ -108,7 +109,7 @@
   <v-card variant="tonal" class="my-4 py-4 px-4 d-flex flex-wrap" style="border-radius: 20px; max-width: 1000px;">
     <v-col md="8" xl="6" class="px-0" >
       <v-spacer class="card-title">
-        <div>
+        <div style=" font-size: 32px; line-height: 39px; padding-bottom: 10px;">
           <span>Akıllı Geçiş Kontrol</span>
             sistemleri daha akıllı bir yaşam tarzına geçiş yapmak için mükemmel bir seçenektir.
       </div>
@@ -116,7 +117,7 @@
         </v-spacer>
         <v-spacer class="card-text">
           <div>
-          <span>
+          <span style=" font-size: 18px;">
           Gelişmiş teknoloji ve kullanıcı dostu tasarımı ile bu sistem, günlük işlerinizi kolaylaştırırken güvende hissetmenizi sağlar. İşletmeler, kamu binaları, apartmanlar ve diğer birçok yer için ideal bir güvenlik çözümü sunar. QR Kod ve RFID okuyucu sistemler ile hem güvenlik hem de erişim kontrolünü optimize edebilirsiniz.
         </span>
       </div>
@@ -176,9 +177,8 @@
 
 <v-spacer class="page d-flex justify-center">
   <v-card variant="tonal" style="border-radius: 20px;  max-width: 1000px;" class=" kontrol-card-color my-4 py-4 px-4 d-flex flex-wrap">
-    <v-row>
-      <v-col md="4" sm="4" xl="6" class="px-0">
-        <v-spacer class="d-flex justify-center my-4 px-4">
+      <v-col md="4" xl="6" class="px-0">
+        <v-spacer class="d-flex justify-center my-4 ">
           <v-sheet width="100%" class="d-flex justify-center align-center" elevation="0" rounded="xl" color="#B0C7BC">
             <v-img
               
@@ -191,7 +191,7 @@
         <v-spacer class="d-flex justify-center my-4">
           <v-sheet
             elevation="0"
-            width="80%"
+            width="100%"
             rounded="xl"
             color="#BBC6C1"
           >
@@ -220,13 +220,13 @@
       </v-col>
       <v-col md="8" xl="6" class="px-0">
         <v-spacer class="card-title">
-          <div>
+          <div style=" font-size: 32px; line-height: 39px; padding-bottom: 10px">
             <span>Akıllı Kontrol Grubu</span> Evinizin kontrolünü parmak uçlarınızda tutun.
           </div>
           </v-spacer>
           <v-spacer class="card-text">
             <div>
-            <span>
+            <span style=" font-size: 18px;">
             Akıllı Kameralar, evinizin, işyerinizin veya herhangi bir mekanın güvenliğini ve gözetimini en üst düzeye çıkarmak için tasarlanmış üstün bir teknoloji ürünüdür. Bu kameralar, gelişmiş özelliklerle donatılmış, yüksek çözünürlüklü video kaydı, hareket algılama, uzaktan erişim ve daha fazlası sunarak size sakinlik ve güvenlik sağlar.
           </span>
         </div>
@@ -239,23 +239,21 @@
           </div>
           </v-spacer>
       </v-col>
-    </v-row>
   </v-card>
 </v-spacer>
 
 
 <v-spacer class="page d-flex justify-center">
   <v-card variant="tonal" style="border-radius: 20px;  max-width: 1000px;" class="my-4 py-4 sensor-card-color px-4 d-flex flex-wrap">
-    <v-row>
-      <v-col md="4" xl="6" class="px-0" >
+      <v-col md="8" xl="6" class="px-0" >
         <v-spacer class="card-title">
-          <div>
+          <div style=" font-size: 32px; line-height: 39px; padding-bottom: 10px;">
             <span>Akıllı Sensörler</span> Ev otomasyonunuza veya işyerinizin güvenliğine entegre edilebilecek çok yönlü cihazlardır.
           </div>
         </v-spacer>
         <v-spacer class="card-text">
             <div>
-              <span>
+              <span style=" font-size: 18px;">
               Gelişmiş teknoloji ve kullanıcı dostu tasarımı ile bu sistem, günlük işlerinizi kolaylaştırırken güvende hissetmenizi sağlar. İşletmeler, kamu binaları, apartmanlar ve diğer birçok yer için ideal bir güvenlik çözümü sunar. QR Kod ve RFID okuyucu sistemler ile hem güvenlik hem de erişim kontrolünü optimize edebilirsiniz.
             </span>
           </div>
@@ -268,11 +266,10 @@
         </div>
         </v-spacer>
       </v-col>
-      <v-col md="8" xl="6" class="px-0" >
+      <v-col md="4" xl="6" class="px-0" >
         <v-spacer class="d-flex justify-center my-4">
-        <v-sheet width="80%" class="d-flex justify-center align-center" elevation="0"  rounded="xl" color="#97A9B7">
+        <v-sheet width="100%" class="d-flex justify-center align-center" elevation="0"  rounded="xl" color="#97A9B7">
           <v-img
-           max-width="400px"
             width="auto"
             aspect-ratio="4/3"
             src="@/assets/sensor.png"
@@ -281,7 +278,7 @@
       </v-spacer>
       <v-spacer class="d-flex justify-center my-4">
           <v-sheet
-            width="80%"
+            width="100%"
             elevation="0"
             
             rounded="xl"
@@ -315,7 +312,6 @@
           </v-sheet>
         </v-spacer>
       </v-col>
-    </v-row>
   </v-card>
 </v-spacer>
 
